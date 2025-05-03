876bcc9a27454d6c16e9b34753fb6a787c8e00be
NgirfanulMangali <qwertymetal0.com>
2025-05-02 18:39:32 -0700

    finsihing making input text using dom

--- script/index.js
+++ script/index.js
@@ -1,5 +1,7 @@
+// Save data in local storage
 const saveData = [];
 
+// Get the input element and add an event listener for the 'keydown' event
 document.getElementById("input").addEventListener('keydown', (e) => {
 
     if (e.key === 'Enter') {
@@ -8,37 +10,30 @@ document.getElementById("input").addEventListener('keydown', (e) => {
         const listResult = document.getElementById("result");
         listResult.innerHTML = "";
         saveData.forEach((item) => {
-            listResult.innerHTML += ` <div class="w-[450px] h-[60px] bg-Very-Dark-Grayish-Blue-alt rounded-t-lg flex px-4 break-all overflow-auto ">
-                        <div class="mt-3">
-                            <div class="w-[30px] h-[30px] rounded-full bg-Very-Dark-Grayish-Blue-alt border-[1px] border-Dark-Grayish-Blue"></div>
-                        </div>
-                        <div class="mx-5">
-                            <p class="text-Light-Grayish-Blue-hover  mt-4">${item} </p>
-                        </div>
-                        <div class="flex items-center justify-center ml-auto">
-                            <button class="cursor-pointer w-[30px] h-[30px] flex items-center justify-center"><img src="/images/icon-cross.svg" alt="icon-cross" class=""></button>
-                        </div>
-                </div>`;
+            listResult.innerHTML += `<div class="w-[450px] h-[60px]     bg-Very-Dark-Grayish-Blue-alt flex px-4 break-all overflow-auto border-b-1 border-Dark-Grayish-Blue">
+                                <div class="mt-3">
+                                    <div class="w-[30px] h-[30px] rounded-full bg-Very-Dark-Grayish-Blue-alt border-[1px] border-Dark-Grayish-Blue"></div>
+                                </div>
+                                <div class="mx-5">
+                                    <p class="text-Light-Grayish-Blue-hover  mt-4">${item} </p>
+                                </div>
+                                <div class="flex items-center justify-center ml-auto">
+                                    <button class="cursor-pointer w-[30px] h-[30px] flex items-center justify-center"><img src="/images/icon-cross.svg" alt="icon-cross" class=""></button>
+                                </div>`;
         });
-        document.getElementById("bawah").innerHTML = `
-                    <div class="w-[450px] h-[60px] bg-Very-Dark-Grayish-Blue-alt rounded-t-lg flex px-4 break-all overflow-auto border-b-1 border-Dark-Grayish-Blue">
-                                    <div class="mt-3">
-                                        <div class="w-[30px] h-[30px] rounded-full bg-Very-Dark-Grayish-Blue-alt border-[1px] border-Dark-Grayish-Blue"></div>
-                                    </div>
-                                    <div class="mx-5">
-                                        <p class="text-Light-Grayish-Blue-hover  mt-4">rrr </p>
-                                    </div>
-                                    <div class="flex items-center justify-center ml-auto">
-                                        <button class="cursor-pointer w-[30px] h-[30px] flex items-center justify-center"><img src="/images/icon-cross.svg" alt="icon-cross" class=""></button>
-                                    </div>
-                            </div>
+        document.getElementById("buttonText").innerHTML = `
+        <div class="w-[450px] h-[60px] bg-Very-Dark-Grayish-Blue-alt  flex justify-between px-4 rounded-b-lg text-Dark-Grayish-Blue ">
+                            <p class="mt-4">5 items left</p>
+                            <button class="cursor-pointer"><p>Clear Completed</p></button>
+                        </div>
 
-                    <div class="w-[450px] h-[60px] bg-Very-Dark-Grayish-Blue-alt  flex items-center justify-center px-4 rounded mt-6">
-                    <div class="flex items-center px-4 text-Dark-Grayish-Blue gap-5">
-                        <button class="cursor-pointer"><p>All</p></button>
-                        <button class="cursor-pointer"><p>Active</p></button>
-                        <button class="cursor-pointer"><p>Completed</p></button>
-                    </div>`;
+                        <div class="w-[450px] h-[60px] bg-Very-Dark-Grayish-Blue-alt  flex items-center justify-center px-4 rounded mt-6">
+                        <div class="flex items-center px-4 text-Dark-Grayish-Blue gap-5">
+                            <button class="cursor-pointer"><p>All</p></button>
+                            <button class="cursor-pointer"><p>Active</p></button>
+                            <button class="cursor-pointer"><p>Completed</p></button>
+                        </div>
+                                        `;
     }
 });
 
